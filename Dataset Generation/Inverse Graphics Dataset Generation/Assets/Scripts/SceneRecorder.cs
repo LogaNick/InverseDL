741aecee1aaae535b1bd175748e2575959bc01c2
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-06-23 23:21:32 -0300

    Renaming CreateExampleLabel to SceneRecorder. Adding documentation to the SceneRecorder class.

--- Dataset Generation/Inverse Graphics Dataset Generation/Assets/Scripts/SceneRecorder.cs
+++ Dataset Generation/Inverse Graphics Dataset Generation/Assets/Scripts/SceneRecorder.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class CreateExampleLabel : MonoBehaviour {
+/// <summary>
+/// Create examples/labels of the current state of the scene. 
+/// Saves out the camera views as well as records any RecordObjects
+/// in the scene. Saves to a json file.
+/// 
+/// In other words, creates "scene snapshots"
+/// </summary>
+public class SceneRecorder : MonoBehaviour {
 
+    // Resolution of our images to save
     public int resolutionWidth = 256;
     public int resolutionHeight = 256;
 
+    /// <summary>
+    /// Test function to save out the current scene snapshot
+    /// </summary>
     [ContextMenu("Save Current Scene Record Snapshot")]
     public void SaveCurrentSnapshot()
     {
         SaveCurrentSceneRecord("../scene_record.json", "../view");
     }
 
+    /// <summary>
+    /// Save the current scene snapshot to a json file
+    /// </summary>
+    /// <param name="fileName">JSON file to create and save to</param>
+    /// <param name="imageFileName">The prefix to the pngs to output.</param>
     public void SaveCurrentSceneRecord(string fileName, string imageFileName)
     {
         string sceneRecordString = JsonUtility.ToJson(RecordSceneSnapshot(imageFileName));
         using (StreamWriter writer = new StreamWriter(fileName))
         {
             writer.Write(sceneRecordString);
-
         }
     }
 
+    /// <summary>
+    /// Gathers all the CameraPNGSavers and RecordObjects. Saves out pngs of the CameraPNGSavers,
+    /// and creates ObjectRecords of the gameobjects that we are interested in saving from the 
+    /// RecordObjects in the scene. 
+    /// 
+    /// Returns a SceneRecord which consists of the image paths as well as all the ObjectRecords
+    /// </summary>
+    /// <param name="imageFileName">Filename path and prefix to save images to</param>
+    /// <returns>Scene record of the image paths as well as the object records in the scene</returns>
 	public SceneRecord RecordSceneSnapshot(string imageFileName="../image")
     {
         // It would be faster to cache these some way, but I don't think it'll be our bottleneck
@@ -55,6 +79,12 @@ public class CreateExampleLabel : MonoBehaviour {
     }
 }
 
+/// <summary>
+/// A scene record is a snapshot of the scene, containing the paths
+/// to the image files that were saved out, and the ObjectRecords 
+/// (containing transformation matrices etc of all the objects we're
+/// interested in saving).
+/// </summary>
 [System.Serializable]
 public class SceneRecord
 {
