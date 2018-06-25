3d89de35fea92df9a5f8fb7d673aad4623eeb949
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-06-25 10:28:33 -0300

    Quickly made extension class for FileExtensions (basically just making saving to a file without having to check and create the directories that might not exist more direct). Fairly hacky

---
+++ Dataset Generation/Inverse Graphics Dataset Generation/Assets/Scripts/Extensions/FileExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// File extensions to simplify writing to files
+/// 
+/// A bit hacky at the moment
+/// </summary>
+public class FileExtensions : MonoBehaviour {
+
+	public static void SaveStringToFile(string fileName, string fileString)
+    {
+        CheckAndCreateDirectories(fileName);
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            writer.Write(fileString);
+        }
+    }
+
+    public static void CheckAndCreateDirectories(string fileName)
+    {
+        string[] parts = fileName.Split('/');
+
+        // Don't want to create a folder if we want to save a file without extension...
+        if(!fileName.EndsWith("/"))
+        {
+            parts[parts.Length - 1] = ".";
+        }
+
+        string concatenatedPath = "";
+        foreach(string part in parts)
+        {
+            // If there's a . in the path, then we'll consider this file name part of the path and break
+            if(part.Contains("."))
+            {
+                break;
+            }
+
+
+            concatenatedPath += part + Path.DirectorySeparatorChar;
+
+            if(!Directory.Exists(concatenatedPath))
+            {
+                Directory.CreateDirectory(concatenatedPath);
+            }
+        }
+    }
+}
