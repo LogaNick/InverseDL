828fb20dfea5bbdbc148a4828d76c417f3187e40
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-06-25 16:29:04 -0300

    Starting to load data in from the json files.

---
+++ Data Import/import_data.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Import generated data
+"""
+
+import json
+from pathlib import Path
+
+def get_json_files_in_directory(directory):
+    """
+    Returns all the json files in the directory
+    """
+    path = Path(directory)
+    json_files = [json_file for json_file in path.iterdir() if json_file.suffix == ".json"]
+    
+    return json_files
+
+def from_json_string(string):
+    """
+    Returns an experiment's data from the given json string
+    """
+    return json.loads(string)
+    
+    
+def from_json_file(path):
+    """
+    Returns the experiment data from the json file at the given path
+    """
+    with open(path) as file:
+        data = json.load(file)
+    return data
+    
+
+def from_directory(directory):
+    """
+    Returns a list of all experiment data in the given directory
+    """
+    data = []
+    for file in get_json_files_in_directory(directory):
+        data.append(from_json_string(file.read_text()))
+        
+    return data
