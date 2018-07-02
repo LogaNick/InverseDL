b457180ffea02bb848cbea2c4ed19a37e3c9fa2c
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-07-02 12:35:24 -0300

    Converting json data into usable tensorflow data.

--- Data Import/convert_data.py
+++ Data Import/convert_data.py
@@ -3,8 +3,7 @@
 Converts json files to usable data for our models
 """
 import tensorflow as tf
-import sys
-sys.path.append('..')
+import numpy as np
 
 # Maybe look into using queues?
 
@@ -19,49 +18,59 @@ def decode_image(image_filename):
     file_contents = tf.read_file(image_filename)
     return tf.image.decode_image(file_contents)
 
+def decode_object_record(record_name, record_data):
+    """
+    Creates the tensorflow representation of the given record_name
+    
+    record_name is a string representing the name of the record (in the json file)
+    record_data is the dictionary that was retrieved from the json file
+    """
+    if record_name is "name":
+        # This is a string (and not a dictionary)
+        return record_data
+    elif record_name is "translation" or record_name is "eulerAngles" or \
+        record_name is "scale":
+        # Translation, eulerAngles, and scale are all 3-dimensional vectors
+        return [record_data['x'], record_data['y'], record_data['z']]
+    elif record_name is "quaternion":
+        # A quaternion is 4 dimensional
+        return [record_data['x'], record_data['y'], record_data['z'], record_data['w']]
+    elif record_name is "translationMatrix" or record_name is "rotationMatrix" or \
+        record_name is "scaleMatrix" or record_name is "transformationMatrix":
+        # A 4x4 matrix (this may need to be transposed)
+        return np.matrix("""
+                         {} {} {} {};
+                         {} {} {} {};
+                         {} {} {} {};
+                         {} {} {} {}""".format(
+                         record_data['e00'], record_data['e01'], record_data['e02'], record_data['e03'],
+                         record_data['e10'], record_data['e11'], record_data['e12'], record_data['e13'],
+                         record_data['e20'], record_data['e21'], record_data['e22'], record_data['e23'],
+                         record_data['e30'], record_data['e31'], record_data['e32'], record_data['e33']))
+        
+        
 def convert_to_tf_variable(vector, name=None, dtype=tf.float32):
     tf.variable(dtype=dtype, )
 
-def convert_data_to_tensors(data, fields=["imageFiles$0"], conversion_functions=[decode_image]):
+def convert_data_to_tensors(data, image_indices=[0], object_records=[]):
     """
     Converts a list of data (as imported by import_data) to a list of
     tensor lists (for each field in fields)
-    
-    Each field in fields is split on $. The parts are then used to as keys
-    or indices to the dicts/lists in the data object
+
     """
-    
-    # If conversion_functions isn't full, extend with Nones
-    conversion_functions.extend([None] * (len(fields) - len(conversion_functions)))
-    
     converted_data = []
-    
-    all_split_fields = []
-    for field in fields:
-        # Split the fields
-        all_split_fields.append(field.split("$"))
     
     for datum in data:
         converted_datum = []
+    
+        for image_index in image_indices:
+            converted_datum.append(decode_image(image_filename=datum['imageFiles'][image_index]))
         
-        current_datum_property = datum
+        # Add in all the object records with the given name (may want to only specify certain indices in the future)
+        for object_record in datum['objectRecords']:
+            for object_record_key in object_records:
+                converted_datum.append(decode_object_record(object_record, object_record[object_record_key]))
+                
+        converted_data.append(converted_datum)
         
-        for split_fields, conversion_function in zip(all_split_fields, conversion_functions):
-            for split_field in split_fields:
-                key_index = None
-                # Going to try to store an int for indexes, otherwise it's a key (string)
-                try:
-                    key_index = int(split_field)
-                except ValueError:
-                    key_index = split_field
-                
-                current_datum_property = current_datum_property[key_index]
-                
-            converted = current_datum_property[key_index]
-            
-            if conversion_function is not None:
-                converted = conversion_function(converted)
-            else:
-                """"""
-            
-            converted_datum.append()
+    return converted_data
