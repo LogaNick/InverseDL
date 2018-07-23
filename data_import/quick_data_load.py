578ef5f7737cefaf0f61624d69192668860dc1fd
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-07-23 20:22:50 -0300

    Adding a function to quickly make a tfrecord.

--- data_import/quick_data_load.py
+++ data_import/quick_data_load.py
@@ -5,7 +5,7 @@ Created on Tue Jul 10 11:21:16 2018
 @author: codej
 """
 from data_import.import_data import from_directory
-from data_import.convert_data import convert_data_to_tensors
+from data_import.convert_data import convert_data_to_tensors, write_tfrecord
 
 def get_examples_labels_from_directory(directory="data/experiment_0/",
                                        quantize=True,
@@ -17,3 +17,11 @@ def get_examples_labels_from_directory(directory="data/experiment_0/",
                                      one_hot=one_hot)
     
     return examples, labels
+
+def create_tfrecord(directory="data_import/data/experiment_0/",
+                                       quantize=True,
+                                       one_hot=True):
+    data = from_directory(directory)
+    write_tfrecord(data, object_records=["translation"],
+                                           quantize=True, 
+                                           one_hot=True)
