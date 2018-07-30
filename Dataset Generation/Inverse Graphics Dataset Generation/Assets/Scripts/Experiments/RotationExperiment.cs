10d98ab79c9820b39e1f0a5020a0b4e8fbbef597
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-07-30 15:15:16 -0300

    Data generation code for rotation.

---
+++ Dataset Generation/Inverse Graphics Dataset Generation/Assets/Scripts/Experiments/RotationExperiment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Experiment for performing linear translations. Linearly translates
+/// the target gameobject between startPosition and endPosition during
+/// its step
+/// </summary>
+public class RotationExperiment : Experiment {
+
+    public Vector3 axis = Vector3.up;
+    public float startAngle = -180f;
+    public float endAngle = 180f;
+
+    public Transform target;
+
+    public override void PerformStep(float percentage, SceneRecorder sceneRecorder)
+    {
+        base.PerformStep(percentage, sceneRecorder);
+
+        target.rotation = Quaternion.Euler(axis * (startAngle + percentage * (endAngle - startAngle)));
+    }
+
+}
