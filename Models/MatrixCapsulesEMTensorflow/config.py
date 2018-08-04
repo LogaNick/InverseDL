d6a63ef353f01a55a67c979d3b1594f587ab7ce9
Nicholas Barreyre <nicklbarreyre@gmail.com>
2018-08-04 15:31:16 -0300

    Changed configs for input of size 48 x 48 (96 was too big in mem.)

--- Models/MatrixCapsulesEMTensorflow/config.py
+++ Models/MatrixCapsulesEMTensorflow/config.py
@@ -61,7 +61,7 @@ def get_coord_add(dataset_name: str):
                               [[8., 12.], [12., 12.], [16., 12.], [24., 12.]],
                               [[8., 16.], [12., 16.], [16., 16.], [24., 16.]],
                               [[8., 24.], [12., 24.], [16., 24.], [24., 24.]]], 32.),
-                'rotation_96' : ([], 96.) # Note: comment out coordinate addition
+                'rotation_48' : ([], 48.) # Note: comment out coordinate addition
                }
     coord_add, scale = options[dataset_name]
 
@@ -73,19 +73,19 @@ def get_coord_add(dataset_name: str):
 def get_dataset_size_train(dataset_name: str):
     options = {'mnist': 55000, 'smallNORB': 23400 * 2,
                'fashion_mnist': 55000, 'cifar10': 50000, 'cifar100': 50000,
-               'translation' : 10201, 'translation_9' : 10201, 'rotation_8' : 8712, 'rotation_96' : 33800}
+               'translation' : 10201, 'translation_9' : 10201, 'rotation_8' : 8712, 'rotation_48' : 33800}
     return options[dataset_name]
 
 
 def get_dataset_size_test(dataset_name: str):
     options = {'mnist': 10000, 'smallNORB': 23400 * 2,
                'fashion_mnist': 10000, 'cifar10': 10000, 'cifar10': 10000,
-               'translation' : 121, 'translation_9' : 121, 'rotation_8' : 0, 'rotation_96' : 0}
+               'translation' : 121, 'translation_9' : 121, 'rotation_8' : 0, 'rotation_48' : 0}
     return options[dataset_name]
 
 
 def get_num_classes(dataset_name: str):
-    options = {'mnist': 10, 'smallNORB': 5, 'fashion_mnist': 10, 'cifar10': 10, 'cifar100': 100, 'translation' : 4, 'translation_9' : 9, 'rotation_8' : 8, 'rotation_96' : 8}
+    options = {'mnist': 10, 'smallNORB': 5, 'fashion_mnist': 10, 'cifar10': 10, 'cifar100': 100, 'translation' : 4, 'translation_9' : 9, 'rotation_8' : 8, 'rotation_48' : 8}
     return options[dataset_name]
 
 
@@ -101,5 +101,5 @@ def get_create_inputs(dataset_name: str, is_train: bool, epochs: int):
                'translation': lambda: create_inputs_translation(is_train, epochs),
                'translation_9': lambda: create_inputs_translation(is_train, epochs),
                'rotation_8': lambda: create_inputs_translation(is_train, epochs),
-               'rotation_96': lambda: create_inputs_translation(is_train, epochs, dim=96)}
+               'rotation_48': lambda: create_inputs_translation(is_train, epochs, dim=48)}
     return options[dataset_name]
