dee58365e41211a8b5d405c657c2b6a928529ad0
Jeff//ZEN_BOOK <jeffreyjfillingham@gmail.com>
2018-08-05 16:23:13 -0300

    Making from_directory recusrive and able to exclude certain directories.

--- data_import/import_data.py
+++ data_import/import_data.py
@@ -6,6 +6,20 @@ Import generated data
 import json
 from pathlib import Path
 
+def get_directories_in_directory(directory, exclude=[]):
+    """
+    Returns all the directories within the directory
+    """
+    path = Path(directory)
+    
+    directories = []
+    
+    for directory in path.iterdir():
+        if directory.name not in exclude and directory.is_dir():
+            directories.append(directory)
+    
+    return directories
+
 def get_json_files_in_directory(directory):
     """
     Returns all the json files in the directory
@@ -31,18 +45,30 @@ def from_json_file(path):
     return data
     
 
-def from_directory(directory, add_directory_to_image_path=True):
+def from_directory(directory, add_directory_to_image_path=True, recursive=False, exclude=[]):
     """
     Returns a list of all experiment data in the given directory
     """
     data = []
-    for file in get_json_files_in_directory(directory):
-        data.append(from_json_string(file.read_text()))
+    
+    files = []
+    
+    if recursive:
+        directories = get_directories_in_directory(directory, exclude)
         
-        if add_directory_to_image_path:
-            # Add the filepath
-            for i in range(len(data[-1]['imageFiles'])):
-                data[-1]['imageFiles'][i] = "{}{}".format(directory, data[-1]['imageFiles'][i])
+        for d in directories:
+            data.extend(from_directory(str(d), add_directory_to_image_path, recursive, exclude))
             
-        
+    
+    files.append(get_json_files_in_directory(directory))
+    for filelist in files:
+        for file in filelist:
+            data.append(from_json_string(file.read_text()))
+            
+            if add_directory_to_image_path:
+                # Add the filepath
+                for i in range(len(data[-1]['imageFiles'])):
+                    data[-1]['imageFiles'][i] = "{}{}".format(directory, data[-1]['imageFiles'][i])
+                
+            
     return data
