63344df2694b13754f888a2d9872cc4cc170fce9
Nick Barreyre <nbarreyre@dal.ca>
2018-08-14 21:46:21 -0300

    ready for report ddl

--- test_model.py
+++ test_model.py
@@ -5,10 +5,12 @@ Created on Tue Jul 17 14:11:10 2018
 @author: codej
 """
 
+import sys
 import Models.MatrixCapsulesEMTensorflow as capsule
 import Models.MatrixCapsulesEMTensorflow.config
 import Models.MatrixCapsulesEMTensorflow.test_2 as test
 
 #examples, labels = get_examples_labels_from_directory("data_import/data/experiment_0/")
 
-test.main([None, "animals_pose"])
+dataset_name = sys.argv[1]
+test.main([None, dataset_name])
--- train_model.py
+++ train_model.py
@@ -5,6 +5,7 @@ Created on Tue Jul 17 14:11:10 2018
 @author: codej
 """
 
+import sys
 from data_import.quick_data_load import get_examples_labels_from_directory
 import Models.MatrixCapsulesEMTensorflow as capsule
 import Models.MatrixCapsulesEMTensorflow.config
@@ -12,4 +13,6 @@ import Models.MatrixCapsulesEMTensorflow.train as train
 
 #examples, labels = get_examples_labels_from_directory("data_import/data/experiment_0/")
 
-train.main([None, "animals_pose"])
+# "animals_pose"
+dataset_name = sys.argv[1]
+train.main([None, dataset_name])
